d20d9d0b59cd33db263f359721e23ffe8ec38575
Kevin <kevin21921@gmail.com>
2015-11-05 08:38:10 -0500

    Modified Devise for Stripe.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  belongs_to :plan
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  belongs_to :plan
+  attr_accessor :stripe_card_token
+
+  def save_with_payment
+    if valid?
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      self.stripe_customer_token = customer.id
+      save!
+    end
+  end
 end
